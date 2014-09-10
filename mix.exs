3bc78cbcd9f5e0b1007c5747bba0ab6ebfafbf02
Roberto Soares <roberto.tech@gmail.com>
2014-09-09 21:53:45 -0300

    added dependencies: ibrowse, httpotion, mock

--- mix.exs
+++ mix.exs
@@ -15,16 +15,11 @@ defmodule Exswift.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.0"},
+      {:httpotion, "~> 0.2.0"},
+      {:mock, github: "jjh42/mock", tag: "v0.0.3"}
+    ]
   end
 end
